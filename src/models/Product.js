91c548a0e332d1fccc3d16dd8501a692ceca49e0
matheus10g <56286084+matheus10g@users.noreply.github.com>
2019-10-14 21:33:44 -0300

    Add files via upload

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    price: Number,
-    photo: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+    },
+    photo: {
+        type: String,
+        required: [true, 'Photo is required'],
+    },
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
